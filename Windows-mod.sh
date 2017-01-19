8b363337aebff8a0d4dfd591428fb01aa5536e70
Pedro Pinto <pedropinto24@hotmail.com>
2017-01-19 15:56:45 +0000

    .

--- Windows-mod.sh
+++ Windows-mod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "use BIVM\ndb.dropDatabase()\n" | mongo
+mongo BIVM --eval "db.dropDatabase()" 
 
-mongoimport /db BIVM /collection maquinas /jsonArray /file datasetMod/maquinas.json
-mongoimport /db BIVM /collection utilizadores /jsonArray /file datasetMod/utilizadores.json
-mongoimport /db BIVM /collection auditMaquinas /jsonArray /file datasetMod/auditMaquinas.json
-mongoimport /db BIVM /collection auditVendas /jsonArray /file datasetMod/auditVendas.json
-mongoimport /db BIVM /collection auditUtilizadores /jsonArray /file datasetMod/auditUtilizadores.json
+mongoimport --db BIVM --collection maquinas --jsonArray --file datasetMod/maquinas.json
+mongoimport --db BIVM --collection utilizadores --jsonArray --file datasetMod/utilizadores.json
+mongoimport --db BIVM --collection auditMaquinas --jsonArray --file datasetMod/auditMaquinas.json
+mongoimport --db BIVM --collection auditVendas --jsonArray --file datasetMod/auditVendas.json
+mongoimport --db BIVM --collection auditUtilizadores --jsonArray --file datasetMod/auditUtilizadores.json
--- Windows-reset.sh
+++ Windows-reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "use BIVM\ndb.dropDatabase()\n" | mongo
+mongo BIVM --eval "db.dropDatabase()" 
 
-mongoimport /db BIVM /collection maquinas /jsonArray /file datasetBase/maquinas.json
-mongoimport /db BIVM /collection utilizadores /jsonArray /file datasetBase/utilizadores.json
+mongoimport --db BIVM --collection maquinas --jsonArray --file datasetBase/maquinas.json
+mongoimport --db BIVM --collection utilizadores --jsonArray --file datasetBase/utilizadores.json
